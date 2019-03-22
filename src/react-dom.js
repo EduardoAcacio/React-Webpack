00a460c5b4baa9f3fea16e9788b91ac4c5246cff
Eduardo Acacio <acaciosh@gmail.com>
2019-03-22 17:16:13 -0300

    Adding files

---
+++ src/react-dom.js
@@ -0,0 +1,10 @@
+'use strict'
+
+import React from 'react'
+import { render as render } from './react-dom'
+import Title from render [B]
+
+render (
+    React.createElement(Title),
+    document.querySelector('[data-js="app"]')
+) 
